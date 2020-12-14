ecd234127f148c6581b990e2063b46886b91822b
jocelyn-amezcua <joceamez02@gmail.com>
2020-12-14 05:17:42 -0700

    almost done with pokemon cards

--- water-pokemon/pokecode.js
+++ water-pokemon/pokecode.js
@@ -1,4 +1,3 @@
-
 async function getAPIData(url) {
     try {
 
@@ -11,7 +10,6 @@ async function getAPIData(url) {
     }
 }
 
-
 function loadPage() {
     getAPIData(`https://pokeapi.co/api/v2/pokemon`).then
     (async (data) => {
@@ -23,29 +21,40 @@ function loadPage() {
     })
 }
 
+
+
+
+/** function loadPage() {
+    getAPIData(`https://pokeapi.co/api/v2/type`).then
+    (async (data) => {
+        for (const type of data.results) {
+            await getAPIData(type.url).then((pokeData) => {
+                populatePokeCard(pokeData)
+            })
+        }
+    })
+} **/
+
 const pokemonGrid = document.querySelector('.pokemonGrid')
 
-//load button
+
+//trying out water button
+
 const loadButton = document.querySelector('button')
-    loadButton.className = 'loadbutton'
 
 loadButton.addEventListener('click', () => {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon/7`).then
+    getAPIData(`https://pokeapi.co/api/v2/pokemon`).then
     (async (data) => {
-        let loadAbilities = document.createElement('ul')
-        data.abilities.forEach(abilities => {
-            console.log(data)
-            let abilityItem = document.createElement('li')
-            abilityItem.textContent = abilities.ability.name
-            loadAbilities.appendChild(abilityItem)
-        });
-        let loadImage = document.createElement('img')
-        loadImage.src = `../pokemon/images/007.png`
-        pokemonGrid.appendChild(loadAbilities)
-        pokemonGrid.appendChild(loadImage)
+        let waterImage = document.createElement('img')
+        waterImage.src = `../pokemon/images/${getImageFileName(pokemon)}.png`
+        pokemonGrid.appendChild(waterImage)
     })
 })
-//load button
+
+
+//end of water button code
+
+
 
 
 function populatePokeCard(pokemon) {
@@ -78,12 +87,9 @@ function populateCardFront(pokemon) {
 function populateCardBack(pokemon) {
     let cardBack = document.createElement('div')
         cardBack.className = `card_face card_face_back`
-    //let backLabel = document.createElement('p')
-    let backImage = document.createElement('img')
-    //backLabel.textContent = `I'm the back of the card`
-    backImage.src = `../pokemon/images/cardback.png`
-    //cardBack.appendChild(backLabel)
-    cardBack.appendChild(backImage)
+    let backLabel = document.createElement('p')
+    backLabel.textContent = `I'm the back of the card`
+    cardBack.appendChild(backLabel)
     return cardBack
 }
 
@@ -96,4 +102,4 @@ function getImageFileName(pokemon) {
 }
 
 
-loadPage()
+//loadPage()
